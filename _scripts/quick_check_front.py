ead68412ece03f55d6af1913a973cbb6052c2f82
Seth Troisi <sethtroisi@google.com>
2022-02-25 14:53:30 -0800

    Add script to check front (yaml) parses

---
+++ _scripts/quick_check_front.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import yaml
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 2, sys.argv
+
+    name = sys.argv[1]
+    assert os.path.isfile(name), name
+
+    with open(name) as f:
+        d = f.readlines()
+
+    front = "".join(d[:d.index("---\n", 1)])
+    assert yaml.safe_load(front)
