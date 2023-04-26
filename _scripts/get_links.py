3082c9cb3f25e88ab6ccbb8bbb3861319fad8db6
Seth Troisi <sethtroisi@google.com>
2023-04-25 21:41:21 -0700

    Add script to create photo and video links from directory

---
+++ _scripts/get_links.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import urllib.parse
+
+BASE_URL = "https://static.cloudygo.com/static/"
+
+def make_links(url_path, paths):
+    set_paths = set(paths)
+    no_thumbnails = []
+
+    for path in paths:
+        url = urllib.parse.quote(os.path.join(BASE_URL, url_path, path))
+
+        name, ext = os.path.splitext(path.lower())
+
+        if ext in (".png", ".jpg"):
+            print(f"![{name}]({url})")
+        elif ext in (".mov",):
+            print(f"""
+<video controls>
+  <source src="{url}"
+  {name}
+</video>
+""")
+        else:
+            print(f"UNKNOWN EXTENSION: {ext!r}")
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 2, sys.argv
+
+    photo_dir = sys.argv[1]
+    static_dir = "/Projects/Sites/static/" # will be under /home/<user>/
+    assert static_dir in photo_dir
+    url_path = photo_dir.split(static_dir)[1]
+
+    paths = os.listdir(photo_dir)
+    make_links(url_path, sorted(paths))
